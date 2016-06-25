d19b9b766112e90364358a214ef0b9ef5cea2d62
Jasper Lammens <lammensjasper@gmail.com>
2016-06-25 10:51:12 +0200

    Updated core to version 8.1.3

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -5,6 +5,7 @@ $config_directories = [];
 $settings['hash_salt'] = 'SCN9schr8NXgykpKIicj-gSYLdZfL90xxXF8fCgJsASVrJwu--gtnOSmtmcBju8wLtZK0gghog';
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['container_yamls'][] = __DIR__ . '/services.private.yml';
 
 if (file_exists(__DIR__ . '/settings.local.php')) {
  include __DIR__ . '/settings.local.php';
